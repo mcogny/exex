18351b1216bb5ef1607bd356848ec94fe7cc38be
mcogny <88964837+mcogny@users.noreply.github.com>
2024-07-10 21:39:04 +0900

    Add files via upload

--- strooptest.js
+++ strooptest.js
@@ -336,7 +336,7 @@ function trialsLoopBegin(trialsLoopScheduler, snapshot) {
       psychoJS: psychoJS,
       nReps: 2, method: TrialHandler.Method.RANDOM,
       extraInfo: expInfo, originPath: undefined,
-      trialList: 'conditions.xlsx',
+      trialList: 'conditions.csv',
       seed: undefined, name: 'trials'
     });
     psychoJS.experiment.addLoop(trials); // add the loop to the experiment
